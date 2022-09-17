06062559f2323e3bad386766d9d27158f8ca8ac9
Ridho Anandamal <a.ridho00@gmail.com>
2022-09-17 23:00:57 +0700

    SASS: Add variables for color, text, spacing based on Figma

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import ButtonText from './components/ButtonText.vue';
+</script>
+
+<template>
+  <ButtonText label="Hello World"></ButtonText>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/ButtonText.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+type size = 'small' | 'normal' | 'large';
+type color = 'dark' | 'brand' | 'blue' | 'red' | 'green' | 'yellow';
+type isIcon = 'none' | 'left' | 'right' | 'both';
+
+interface IButtonProps {
+  label: string;
+  size?: size;
+  color?: color;
+  isIcon?: isIcon;
+  iconName?: string;
+}
+
+interface IButtonEmits {
+  click?: string;
+}
+
+const props = defineProps<IButtonProps>();
+const emits = defineEmits<IButtonEmits>();
+
+let arrayClass: string[] = [];
+
+const classGenerator = (): string[] => {
+  return arrayClass;
+};
+
+classGenerator();
+</script>
+
+<template>
+  <button class="button button--small button--brand" @click="emits.click">{{ props.label }}</button>
+</template>
+
+<style scoped lang="scss">
+@import '@/assets/sass/index.scss';
+
+$light: $white;
+
+.button {
+  border: none;
+  outline: none;
+}
+
+// Button Size
+.button--small {
+  padding: 0.5rem 1rem;
+  border-radius: 0.25rem;
+}
+
+// Button Color
+.button--brand {
+  background-color: $teal-800;
+  color: $light;
+}
+</style>
