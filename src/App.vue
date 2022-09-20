af7316a122bdf04ea519695a37ebaaff716d1661
Ridho Anandamal <a.ridho00@gmail.com>
2022-09-20 21:08:02 +0700

    [Components] Update button text  style

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <script setup lang="ts">
-import ButtonText from './components/ButtonText.vue';
+import ButtonText from '@/components/ButtonText.vue';
 </script>
 
 <template>
-  <ButtonText label="Hello World"></ButtonText>
+  <div class="center">
+    <ButtonText>Ini Slot</ButtonText>
+    <ButtonText name-right-icon="mongodb" style-button="blue" type-button="fill">Blue Flat</ButtonText>
+  </div>
 </template>
 
-<style>
+<style lang="scss">
+@import '@/assets/sass/index.scss';
+
+$font-family: $plus-jakarta-sans;
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: $font-family;
+}
+
+.center {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  gap: 16px;
 }
 </style>
--- src/components/ButtonText.vue
+++ src/components/ButtonText.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
-type size = 'small' | 'normal' | 'large';
-type color = 'dark' | 'brand' | 'blue' | 'red' | 'green' | 'yellow';
-type isIcon = 'none' | 'left' | 'right' | 'both';
+type typeButton = 'fill' | 'outline' | 'flat';
+type sizeButton = 'x-small' | 'small' | 'normal' | 'large' | 'x-large';
+type styleButton = 'teal' | 'blue' | 'red' | 'green' | 'yellow';
 
 interface IButtonProps {
-  label: string;
-  size?: size;
-  color?: color;
-  isIcon?: isIcon;
-  iconName?: string;
+  label?: string;
+  typeButton?: typeButton;
+  sizeButton?: sizeButton;
+  styleButton?: styleButton;
+  nameLeftIcon?: string;
+  nameRightIcon?: string;
 }
 
 interface IButtonEmits {
@@ -18,38 +19,142 @@ interface IButtonEmits {
 const props = defineProps<IButtonProps>();
 const emits = defineEmits<IButtonEmits>();
 
-let arrayClass: string[] = [];
+const classGenerator = (): string => {
+  const prefix = `button`;
+  let arrayClass: string[] = [];
 
-const classGenerator = (): string[] => {
-  return arrayClass;
+  arrayClass.push(`${prefix}`);
+  arrayClass.push(props.sizeButton == null ? `${prefix}--normal` : `${prefix}--${props.sizeButton}`);
+
+  if (props.styleButton == null || props.typeButton == null) {
+    arrayClass.push(`${prefix}--teal-fill`);
+  } else {
+    arrayClass.push(`${prefix}--${props.styleButton}-${props.typeButton}`);
+  }
+  return arrayClass.join(' ');
 };
 
-classGenerator();
+const classList = classGenerator();
 </script>
 
 <template>
-  <button class="button button--small button--brand" @click="emits.click">{{ props.label }}</button>
+  <button :class="classList" @click="emits.click">
+    <div class="button__content">
+      <i v-if="props.nameLeftIcon" :class="`bx bxl-${props.nameLeftIcon}`"></i>
+      <span v-if="props.label">{{ props.label }}</span>
+      <span v-else><slot></slot></span>
+      <i v-if="props.nameRightIcon" :class="`bx bxl-${props.nameRightIcon}`"></i>
+    </div>
+  </button>
 </template>
 
 <style scoped lang="scss">
 @import '@/assets/sass/index.scss';
 
-$light: $white;
+@mixin size-class($size, $font-size, $font-weight, $line-height, $x-padding, $y-padding, $border-radius) {
+  &--#{$size} {
+    font-size: $font-size;
+    font-weight: $font-weight;
+    line-height: $line-height;
+    padding: $y-padding $x-padding;
+    border-radius: $border-radius;
+  }
+}
+
+@mixin style-class($style, $color, $active-color, $hover-color, $pressed-color, $disabled-color) {
+  &--#{$style} {
+    &-fill {
+      color: $color;
+      background-color: $active-color;
+
+      &:hover {
+        background-color: $hover-color;
+      }
+
+      &:active {
+        background-color: $pressed-color;
+        outline: 6px solid opacity-color($active-color, 0.25);
+      }
+
+      &:disabled {
+        background-color: $disabled-color;
+      }
+    }
+
+    &-outline {
+      color: $active-color;
+      border-color: $active-color;
+
+      &:hover {
+        color: $hover-color;
+        border-color: $hover-color;
+      }
+
+      &:active {
+        color: $pressed-color;
+        border-color: $pressed-color;
+        background-color: $color;
+        outline: 6px solid opacity-color($active-color, 0.25);
+      }
+
+      &:disabled {
+        color: $disabled-color;
+        border-color: $disabled-color;
+      }
+    }
+
+    &-flat {
+      color: $active-color;
+      background-color: $color;
+
+      &:hover {
+        color: $hover-color;
+      }
+
+      &:active {
+        color: $pressed-color;
+        outline: 6px solid opacity-color($active-color, 0.25);
+      }
+
+      &:disabled {
+        color: $disabled-color;
+      }
+    }
+  }
+}
 
 .button {
   border: none;
   outline: none;
-}
+  cursor: pointer;
+  transition: all 200ms ease-in-out;
 
-// Button Size
-.button--small {
-  padding: 0.5rem 1rem;
-  border-radius: 0.25rem;
-}
+  // Size x-small
+  @include size-class('x-small', $font-size-12, $font-weight-700, $line-height-150, $space-8, $space-4, $space-2);
+  // Size small
+  @include size-class('small', $font-size-14, $font-weight-700, $line-height-150, $space-8, $space-4, $space-2);
+  // Size normal
+  @include size-class('normal', $font-size-16, $font-weight-700, $line-height-140, $space-12, $space-6, $space-4);
+  // Size large
+  @include size-class('large', $font-size-18, $font-weight-700, $line-height-140, $space-16, $space-8, $space-6);
+  // Size x-large
+  @include size-class('x-large', $font-size-20, $font-weight-700, $line-height-140, $space-20, $space-12, $space-6);
 
-// Button Color
-.button--brand {
-  background-color: $teal-800;
-  color: $light;
+  // Style teal
+  @include style-class('teal', $white, $teal-500, $teal-600, $teal-700, $teal-300);
+  // Style blue
+  @include style-class('blue', $white, $blue-500, $blue-600, $blue-700, $blue-300);
+  // Style green
+  @include style-class('green', $white, $green-500, $green-600, $green-700, $green-300);
+  // Style red
+  @include style-class('red', $white, $red-500, $red-600, $red-700, $red-300);
+  // Style yellow
+  @include style-class('yellow', $white, $yellow-500, $yellow-600, $yellow-700, $yellow-300);
+
+  &__content {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
 }
 </style>
